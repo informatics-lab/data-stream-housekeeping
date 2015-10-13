5a60666d250e2f7ee155582c5635b8459c0c1e9c
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2015-10-13 09:51:29 +0100

    Added logging

--- thredds_data_stream_housekeeping.py
+++ thredds_data_stream_housekeeping.py
@@ -7,7 +7,7 @@ import os, time, sys
 import os.path
 
 MAX_FILE_AGE = os.getenv("MAX_FILE_AGE") or (24 * 60 * 60) # In seconds
-SLEEP_TIME = 15 * 60 # In seconds
+SLEEP_TIME = os.getenv("SLEEP_TIME") or (15 * 60) # In seconds
 PATH = os.getenv('DATA_DIR')
 NOW = time.time()
 exceptions = os.getenv("EXCEPTIONS") ? os.getenv("EXCEPTIONS").split(",") : []
@@ -18,7 +18,9 @@ def main():
         f = os.path.join(PATH, f)
         if os.stat(f).st_mtime < NOW - MAX_FILE_AGE:
             if os.path.isfile(f):
-                os.remove(os.path.join(PATH, f))
+                file_to_remove = os.path.join(PATH, f)
+                print "Removing " + file_to_remove
+                os.remove(file_to_remove)
 
     time.sleep(SLEEP_TIME)
 
